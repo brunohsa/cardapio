0bb104eb729a42e44121c76657cf37ba148ec60f
Bruno <brunohsa@hotmail.com>
2020-11-17 22:46:20 -0300

    Ajustes

--- src/main/kotlin/br/com/unip/cardapio/repository/ICardapioRepository.kt
+++ src/main/kotlin/br/com/unip/cardapio/repository/ICardapioRepository.kt
@@ -11,6 +11,10 @@ interface ICardapioRepository : MongoRepository<Cardapio, String> {
 
     fun findByAtivo(ativo: Boolean = true): Optional<Cardapio>
 
+    fun findByUuidFornecedorAndAtivo(uuidFornecedor: String, ativo: Boolean = true): Optional<Cardapio>
+
+    fun countByUuidFornecedor(uuidFornecedor: String): Int
+
     @Query(value = "{ 'uuidFornecedor' : {'\$in' : ?0 }, 'ativo' : true }")
     fun buscarCardapiosAtivosPorFornecedoresUUID(fornecedoresIds: List<String>): List<Cardapio>
 }
